7542589d3b4142f30f3afd461eeeacc6ad57b8c9
Jonatas Rodrigues <mizaelrodriguesworkspace@gmail.com>
2021-12-17 00:56:31 -0300

    Primeiro commit

---
+++ crud/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿using System.Windows.Forms;
+
+namespace crud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btncadastrar = new System.Windows.Forms.Button();
+            this.btnpesquisar = new System.Windows.Forms.Button();
+            this.btnalterar = new System.Windows.Forms.Button();
+            this.btnexcluir = new System.Windows.Forms.Button();
+            this.Cadastros = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txttelefone = new System.Windows.Forms.MaskedTextBox();
+            this.txtcpf = new System.Windows.Forms.MaskedTextBox();
+            this.txtnascimento = new System.Windows.Forms.MaskedTextBox();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtendereço = new System.Windows.Forms.TextBox();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtsalario = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtcpfpesquisa = new System.Windows.Forms.MaskedTextBox();
+            this.Cadastros.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btncadastrar
+            // 
+            this.btncadastrar.Location = new System.Drawing.Point(12, 298);
+            this.btncadastrar.Name = "btncadastrar";
+            this.btncadastrar.Size = new System.Drawing.Size(182, 23);
+            this.btncadastrar.TabIndex = 0;
+            this.btncadastrar.Text = "Cadastrar";
+            this.btncadastrar.UseVisualStyleBackColor = true;
+            this.btncadastrar.Click += new System.EventHandler(this.btncadastrar_Click);
+            // 
+            // btnpesquisar
+            // 
+            this.btnpesquisar.Location = new System.Drawing.Point(200, 298);
+            this.btnpesquisar.Name = "btnpesquisar";
+            this.btnpesquisar.Size = new System.Drawing.Size(182, 23);
+            this.btnpesquisar.TabIndex = 0;
+            this.btnpesquisar.Text = "Pesquisar";
+            this.btnpesquisar.UseVisualStyleBackColor = true;
+            this.btnpesquisar.Click += new System.EventHandler(this.btnpesquisar_Click);
+            // 
+            // btnalterar
+            // 
+            this.btnalterar.Location = new System.Drawing.Point(388, 298);
+            this.btnalterar.Name = "btnalterar";
+            this.btnalterar.Size = new System.Drawing.Size(182, 23);
+            this.btnalterar.TabIndex = 0;
+            this.btnalterar.Text = "Alterar";
+            this.btnalterar.UseVisualStyleBackColor = true;
+            this.btnalterar.Click += new System.EventHandler(this.btnalterar_Click);
+            // 
+            // btnexcluir
+            // 
+            this.btnexcluir.Location = new System.Drawing.Point(578, 298);
+            this.btnexcluir.Name = "btnexcluir";
+            this.btnexcluir.Size = new System.Drawing.Size(182, 23);
+            this.btnexcluir.TabIndex = 0;
+            this.btnexcluir.Text = "Excluir";
+            this.btnexcluir.UseVisualStyleBackColor = true;
+            this.btnexcluir.Click += new System.EventHandler(this.btnexcluir_Click);
+            // 
+            // Cadastros
+            // 
+            this.Cadastros.Controls.Add(this.tabPage2);
+            this.Cadastros.Location = new System.Drawing.Point(12, 12);
+            this.Cadastros.Name = "Cadastros";
+            this.Cadastros.SelectedIndex = 0;
+            this.Cadastros.Size = new System.Drawing.Size(746, 280);
+            this.Cadastros.TabIndex = 1;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtcpfpesquisa);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.txttelefone);
+            this.tabPage2.Controls.Add(this.txtcpf);
+            this.tabPage2.Controls.Add(this.txtnascimento);
+            this.tabPage2.Controls.Add(this.txtnome);
+            this.tabPage2.Controls.Add(this.txtendereço);
+            this.tabPage2.Controls.Add(this.txtid);
+            this.tabPage2.Controls.Add(this.txtsalario);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(738, 251);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "CadastrarPesquisarAlterarExcluir";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(254, 37);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 16);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "ID";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(88, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(186, 16);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Digite um CPF para pesquisar";
+            // 
+            // txttelefone
+            // 
+            this.txttelefone.Location = new System.Drawing.Point(91, 226);
+            this.txttelefone.Mask = "(00) 00000-0000";
+            this.txttelefone.Name = "txttelefone";
+            this.txttelefone.Size = new System.Drawing.Size(109, 22);
+            this.txttelefone.TabIndex = 3;
+            // 
+            // txtcpf
+            // 
+            this.txtcpf.Location = new System.Drawing.Point(91, 198);
+            this.txtcpf.Mask = "000.000.000-00";
+            this.txtcpf.Name = "txtcpf";
+            this.txtcpf.Size = new System.Drawing.Size(109, 22);
+            this.txtcpf.TabIndex = 2;
+            // 
+            // txtnascimento
+            // 
+            this.txtnascimento.Location = new System.Drawing.Point(91, 170);
+            this.txtnascimento.Mask = "00/00/0000";
+            this.txtnascimento.Name = "txtnascimento";
+            this.txtnascimento.Size = new System.Drawing.Size(109, 22);
+            this.txtnascimento.TabIndex = 2;
+            this.txtnascimento.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(91, 86);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(641, 22);
+            this.txtnome.TabIndex = 1;
+            // 
+            // txtendereço
+            // 
+            this.txtendereço.Location = new System.Drawing.Point(91, 114);
+            this.txtendereço.Name = "txtendereço";
+            this.txtendereço.Size = new System.Drawing.Size(641, 22);
+            this.txtendereço.TabIndex = 1;
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(292, 34);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(109, 22);
+            this.txtid.TabIndex = 1;
+            // 
+            // txtsalario
+            // 
+            this.txtsalario.Location = new System.Drawing.Point(91, 142);
+            this.txtsalario.Name = "txtsalario";
+            this.txtsalario.Size = new System.Drawing.Size(190, 22);
+            this.txtsalario.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 16);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Endereço";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 229);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Telefone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Salário";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "CPF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nascimento";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // txtcpfpesquisa
+            // 
+            this.txtcpfpesquisa.Location = new System.Drawing.Point(292, 6);
+            this.txtcpfpesquisa.Mask = "000.000.000-00";
+            this.txtcpfpesquisa.Name = "txtcpfpesquisa";
+            this.txtcpfpesquisa.Size = new System.Drawing.Size(109, 22);
+            this.txtcpfpesquisa.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 333);
+            this.Controls.Add(this.Cadastros);
+            this.Controls.Add(this.btnexcluir);
+            this.Controls.Add(this.btnalterar);
+            this.Controls.Add(this.btnpesquisar);
+            this.Controls.Add(this.btncadastrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Cadastros.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btncadastrar;
+        private System.Windows.Forms.Button btnpesquisar;
+        private System.Windows.Forms.Button btnalterar;
+        private System.Windows.Forms.Button btnexcluir;
+        private System.Windows.Forms.TabControl Cadastros;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MaskedTextBox txtcpf;
+        private System.Windows.Forms.MaskedTextBox txtnascimento;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtendereço;
+        private System.Windows.Forms.TextBox txtsalario;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox txttelefone;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtid;
+        private MaskedTextBox txtcpfpesquisa;
+    }
+}
+
---
+++ crud/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace crud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncadastrar_Click(object sender, EventArgs e)
+        {
+            string strcon = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+Application.StartupPath+@"\empresa.accdb";
+            string query = "INSERT INTO funcionarios(nome,nascimento,cpf,salario,telefone,endereço)values(@nome,@nascimento,@cpf,@salario,@telefone,@endereço)";
+
+            OleDbConnection olecon = new OleDbConnection(strcon);
+
+            OleDbCommand comando = new OleDbCommand(query, olecon);
+
+            comando.Parameters.Add("@nome", OleDbType.VarChar).Value = txtnome.Text;
+            comando.Parameters.Add("@nascimento", OleDbType.VarChar).Value = txtnascimento.Text;
+            comando.Parameters.Add("@cpf", OleDbType.VarChar).Value = txtcpf.Text;
+            comando.Parameters.Add("@salario", OleDbType.VarChar).Value = decimal.Parse(txtsalario.Text);
+            comando.Parameters.Add("@telefone", OleDbType.VarChar).Value = txttelefone.Text;
+            comando.Parameters.Add("@endereço", OleDbType.VarChar).Value = txtendereço.Text;
+
+            try
+            {
+                olecon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Cadastro realizado!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                olecon.Close();
+            }
+        }
+
+        private void btnpesquisar_Click(object sender, EventArgs e)
+        {
+            string strcon = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + @"\empresa.accdb";
+            string query = "select *  from funcionarios where cpf =@cpf";
+
+            OleDbConnection olecon = new OleDbConnection(strcon);
+
+            OleDbCommand comando = new OleDbCommand(query, olecon);
+
+            comando.Parameters.Add("@cpf", OleDbType.VarChar).Value = txtcpfpesquisa.Text;
+
+            try
+            {
+                if (txtcpfpesquisa.Text == "")
+                {
+                    throw new Exception("Você precisa digitar um CPF para pesquisar!");
+                }
+
+                olecon.Open();
+
+                OleDbDataReader dr = comando.ExecuteReader();
+
+                if (dr.HasRows == false)
+                {
+                    throw new Exception("CPF não cadastrado!");
+                }
+                else
+                {
+                    dr.Read();
+
+                    txtid.Text = Convert.ToString(dr["id_funcionario"]);
+                    txtnome.Text = Convert.ToString(dr["nome"]);
+                    txttelefone.Text = Convert.ToString(dr["telefone"]);
+                    txtnascimento.Text = Convert.ToString(dr["nascimento"]);
+                    txtcpf.Text = Convert.ToString(dr["cpf"]);
+                    txtsalario.Text = Convert.ToString(dr["salario"]);
+                    txtendereço.Text = Convert.ToString(dr["endereço"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                olecon.Close();
+            }
+        }
+
+        private void btnalterar_Click(object sender, EventArgs e)
+        {
+            string strcon = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + @"\empresa.accdb";
+            string query = "update funcionarios set nome=@nome,nascimento=@nascimento,cpf=@cpf,salario=@salario,telefone=@telefone,endereço=@endereço where cpf=@cpf";
+
+            OleDbConnection olecon = new OleDbConnection(strcon);
+
+            OleDbCommand comando = new OleDbCommand(query, olecon);
+
+            comando.Parameters.Add("@nome", OleDbType.VarChar).Value = txtnome.Text;
+            comando.Parameters.Add("@nascimento", OleDbType.VarChar).Value = txtnascimento.Text;
+            comando.Parameters.Add("@cpf", OleDbType.VarChar).Value = txtcpf.Text;
+            comando.Parameters.Add("@salario", OleDbType.VarChar).Value = decimal.Parse(txtsalario.Text);
+            comando.Parameters.Add("@telefone", OleDbType.VarChar).Value = txttelefone.Text;
+            comando.Parameters.Add("@endereço", OleDbType.VarChar).Value = txtendereço.Text;
+
+            try
+            {
+                olecon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Alteração concluida!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                olecon.Close();
+            }
+        }
+
+        private void btnexcluir_Click(object sender, EventArgs e)
+        {
+            string strcon = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + Application.StartupPath + @"\empresa.accdb";
+
+            if (MessageBox.Show("Você realmente deseja excluir esse funcionário?",
+                "Cuidado!", MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                MessageBox.Show("Operação Cancelada!");
+            }
+            else
+            {
+                string query = "delete from funcionarios where cpf = @cpf";
+
+                OleDbConnection olecon = new OleDbConnection(strcon);
+
+                OleDbCommand comando = new OleDbCommand(query, olecon);
+
+                comando.Parameters.Add("@cpf", OleDbType.VarChar).Value = txtcpf.Text;
+
+                try
+                {
+                    olecon.Open();
+                    comando.ExecuteNonQuery();
+                    MessageBox.Show("Registro excluido!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    olecon.Close();
+                }
+            }
+        }
+    }
+}
